9b11287fdb4b40a2425cc9cf7ddb55b937859d73
vmcustodio <van.custodio.vc@gmail.com>
2022-08-30 23:27:42 -0300

    add components

---
+++ src/components/ProdutosBuscar.vue
@@ -0,0 +1,13 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: "ProdutosBuscar"
+}
+</script>
+
+<style>
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,29 @@
 <template>
-  <div >
-
-  </div>
+  <section>
+    <h1>Compre ou Venda</h1>
+    <ProdutosLista/>
+    <ProdutosBuscar/>
+  </section>
 </template>
 
 <script>
+import ProdutosBuscar from '@/components/ProdutosBuscar.vue'
+import ProdutosLista from '@/components/ProdutosLista.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    
+    ProdutosBuscar,
+    ProdutosLista
   }
 }
 </script>
+
+<style scoped>
+h1 {
+  color: #87f;
+  margin-top: 40px;
+  font-size: 2rem;
+  text-align: center;
+}
+</style>
