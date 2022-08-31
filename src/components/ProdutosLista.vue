88cbe71f8a0ba00f5829ba3c79513e6d48f91f24
vmcustodio <van.custodio.vc@gmail.com>
2022-08-30 23:41:50 -0300

    add api.json

--- src/components/ProdutosLista.vue
+++ src/components/ProdutosLista.vue
@@ -1,10 +1,32 @@
 <template>
-  <div></div>
+  <section class="produtos-container">
+    <div v-for="produto in produtos" :key="produto.id">
+      <img v-if="produto.fotos" :src="produto.fotos[0].src" :alt="produto.fotos[0]">
+      <p class="preco">{{produto.preco}}</p>
+      <h2 class="titulo">{{produto.nome}}</h2>
+      <p>{{produto.descricao}}</p>
+    </div>
+  </section>
 </template>
 
 <script>
 export default {
-  name: "ProdutosLista"
+  name: "ProdutosLista",
+  data() {
+    return {
+      produtos: null
+    }
+  },
+  methods: {
+    getProdutos() {
+      fetch("http://localhost:3000/produto").then(response => response.json()).then(response => {
+        this.produtos = response
+      })
+    }
+  },
+  created() {
+    this.getProdutos()
+  }
 }
 </script>
 
