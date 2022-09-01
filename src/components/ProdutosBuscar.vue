24c7cf2da69c098d712c1b2df57c2a2de647dfa5
vmcustodio <van.custodio.vc@gmail.com>
2022-09-01 00:02:57 -0300

    add busca

--- src/components/ProdutosBuscar.vue
+++ src/components/ProdutosBuscar.vue
@@ -1,10 +1,23 @@
 <template>
-  <div></div>
+  <form>
+    <input type="text" name="busca" v-model="busca" id="busca">
+    <input type="submit" id="lupa" value="Buscar" @click.prevent="buscarProdutos">
+  </form>
 </template>
 
 <script>
 export default {
-  name: "ProdutosBuscar"
+  name: "ProdutosBuscar",
+  data() {
+    return {
+      busca: ""
+    }
+  },
+  methods: {
+    buscarProdutos() {
+      this.$router.push({ query: { q: this.busca } })
+    }
+  }
 }
 </script>
 
--- src/components/ProdutosLista.vue
+++ src/components/ProdutosLista.vue
@@ -11,19 +11,32 @@
 
 <script>
 import { api } from "@/services.js"
+import { serialize } from "@/helpers.js"
 
 export default {
   name: "ProdutosLista",
   data() {
     return {
-      produtos: null
+      produtos: null,
+      produtosPorPagina: 9,
+    }
+  },
+  computed: {
+    url() {
+      const query = serialize(this.$route.query)
+      return `/produto?_limit=${this.produtosPorPagina}${query}`
     }
   },
   methods: {
     getProdutos() {
-      api.get("/produto").then(response => {
+      api.get(this.url).then(response => {
         this.produtos = response.data
       })
+    }
+  },
+  watch: {
+    url() { // toda vez q a url mudar, vai ter q ativar pra buscar um produto novo
+      this.getProdutos()
     }
   },
   created() {
