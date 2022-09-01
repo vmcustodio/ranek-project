585375dabf29bcbcf888003bd4f56922805ad48a
vmcustodio <van.custodio.vc@gmail.com>
2022-08-31 23:34:15 -0300

    add axios

--- src/components/ProdutosLista.vue
+++ src/components/ProdutosLista.vue
@@ -10,6 +10,8 @@
 </template>
 
 <script>
+import { api } from "@/services.js"
+
 export default {
   name: "ProdutosLista",
   data() {
@@ -19,8 +21,8 @@ export default {
   },
   methods: {
     getProdutos() {
-      fetch("http://localhost:3000/produto").then(response => response.json()).then(response => {
-        this.produtos = response
+      api.get("/produto").then(response => {
+        this.produtos = response.data
       })
     }
   },
