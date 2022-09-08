562a2047536fe7e0a35c790a3f37e838e4b87000
vmcustodio <van.custodio.vc@gmail.com>
2022-09-07 23:14:32 -0300

    add input style

--- src/components/ProdutosBuscar.vue
+++ src/components/ProdutosBuscar.vue
@@ -1,6 +1,6 @@
 <template>
   <form>
-    <input type="text" name="busca" v-model="busca" id="busca">
+    <input type="text" name="busca" v-model="busca" id="busca" placeholder="Buscar...">
     <input type="submit" id="lupa" value="Buscar" @click.prevent="buscarProdutos">
   </form>
 </template>
@@ -21,6 +21,34 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+form {
+  max-width: 600px;
+  margin: 30px auto 60px auto;
+  position: relative;
+}
 
+#busca {
+  width: 100%;
+  padding: 20px;
+  border: none;
+}
+
+#busca:focus,
+#busca:hover {
+  transform: scale(1.1);
+}
+
+#lupa {
+  width: 62px;
+  height: 62px;
+  background: url("../assets/search.svg") no-repeat center center;
+  text-indent: -150px;
+  border: none;
+  cursor: pointer;
+  position: absolute;
+  top: 0px;
+  right: 0px;
+  box-shadow: none;
+}
 </style>
