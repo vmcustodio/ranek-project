ccf312616948de33201e6922ca02699fff96f8fd
vmcustodio <van.custodio.vc@gmail.com>
2022-09-13 23:10:40 -0300

    add user style

--- src/views/usuario/UsuarioView.vue
+++ src/views/usuario/UsuarioView.vue
@@ -38,6 +38,45 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+.usuario {
+  display: grid;
+  grid-template-columns: minmax(140px, 200px) 1fr;
+  max-width: 900px;
+  margin: 40px auto;
+  grid-gap: 30px;
+  padding: 20px;
+}
 
+@media screen and (max-width: 500px) {
+  .usuario {
+    grid-template-columns: 1fr;
+    margin: 0px auto;
+  }
+}
+
+.sidenav a,
+.sidenav button {
+  padding: 10px;
+  display: block;
+  background: #f4f7fc;
+  margin-bottom: 10px;
+  border-radius: 4px;
+}
+
+.sidenav a.router-link-exact-active,
+.sidenav a:hover,
+.sidenav button:hover {
+  background: #87f;
+  color: #fff;
+}
+
+.sidenav button {
+  border: none;
+  width: 100%;
+  font-size: 1rem;
+  text-align: left;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  cursor: pointer;
+}
 </style>
