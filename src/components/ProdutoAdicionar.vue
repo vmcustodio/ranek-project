79cd59ec0e98fd11a834e28336e1943d93b53616
vmcustodio <van.custodio.vc@gmail.com>
2022-09-20 23:07:14 -0300

    add prodcut

--- src/components/ProdutoAdicionar.vue
+++ src/components/ProdutoAdicionar.vue
@@ -1,13 +1,56 @@
 <template>
-  <div></div>
+  <form class="adicionar-produto">
+    <label for="nome">Nome</label>
+    <input id="nome" name="nome" type="text" v-model="produto.nome">
+    <label for="preco">Preço (R$)</label>
+    <input id="preco" name="preco" type="number" v-model="produto.preco">
+    <label for="fotos">Fotos</label>
+    <input id="fotos" name="fotos" type="file" ref="fotos">
+    <label for="preco">Descrição</label>
+    <textarea id="preco" name="preco" v-model="produto.descricao"></textarea>
+    <input type="button" class="btn" value="Adicionar Produto" @click.prevent="adicionarProduto">
+  </form>
 </template>
 
 <script>
-export default {
+import {api} from "@/services.js"
 
+export default {
+  name: "ProdutoAdicionar",
+  data() {
+    return {
+      produto: {
+        nome: "",
+        preco: "",
+        descricao: "",
+        fotos: null
+      }
+    }
+  },
+  methods: {
+    formatarProduto() {
+      this.produto.usuario_id = this.$store.state.usuario.id
+    },
+    adicionarProduto() {
+      this.formatarProduto()
+      api.post("/produto", this.produto).then(() => {
+        this.$store.dispatch("getUsuarioProdutos")
+      })
+    }
+  }
 }
 </script>
 
-<style>
+<style scoped>
+.adicionar-produto {
+  display: grid;
+  grid-template-columns: 100px 1fr;
+  align-items: center;
+  margin-bottom: 60px;
+}
+
+.btn {
+  grid-column: 2;
+}
 
 </style>
